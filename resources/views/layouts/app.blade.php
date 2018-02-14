8dac1b7c805b3158484af3dc7d9fa6db6eec6b36
Tam Huynh <tamhuynhqt@gmail.com>
2018-02-14 17:35:03 +1100

    Laravel initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Car shop @yield('title')</title>
+    
+</head>
+<body>
+  @include('partials.header')
+    <div class="container">
+        @yield('contain')
+    </div>
+    <p>app file</p>
+</body>
+</html>
